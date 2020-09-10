55851bb5d459601dcfb6be5cea2d5092a0ee65cc
ThashinaSultana <70582506+ThashinaSultana@users.noreply.github.com>
2020-09-10 09:40:52 +0530

    Add files via upload

---
+++ Date format.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Date
+    {
+        //private const string d = "MM/dd/yyyy";
+
+        public static void D()
+        {
+            var dateTime = "01-06-2020 12:12:12";
+            DateTime dt = DateTime.ParseExact(dateTime, "MM-dd-yyyy hh:mm:ss", CultureInfo.InvariantCulture);
+            var convertedDate = dt.ToString("dd/MM/yyyy");
+            Console.WriteLine(convertedDate);
+
+        }
+
+        
+    }
+}
---
+++ Filter and store.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+   class Filter
+    {
+
+   public static void Fil()
+    {
+
+            List<string> words = new List<string> { "ajeeth@impelsys.com",
+                "thashina@impelsys.co.in",
+                "shan@impelsys.com",
+                "sam@gmail.co.in",
+                "maimu@impelsys.com"
+                 };
+
+          var filtered1 = new List<string>();
+           var filtered2 = new List<string>();
+
+
+            foreach (var word in words)
+            {
+                if (word.EndsWith(".com"))
+                {
+                    filtered1.Add(word);
+                }
+                if (word.EndsWith(".co.in"))
+                {
+                    filtered2.Add(word);
+                }
+
+            }
+            Console.WriteLine("email ids ending with .com are as follows");
+
+            Console.WriteLine(string.Join("\n", filtered1));
+            Console.WriteLine("email ids ending with .co.in are as follows");
+            Console.WriteLine(string.Join("\n", filtered2));
+            Console.Read();
+
+        }
+}
+    }
+
